b88d1c1e23da8e9a5c9710fe090440647fc5267e
Jean-Baptiste Laval <jean-baptiste.laval@student.ecp.fr>
2022-01-01 13:49:18 +0100

    update dummy output

--- src/dummy.py
+++ src/dummy.py
@@ -1,65 +1,54 @@
 dummy_output1 = {
-    "marmiton": [
-        {
-            "weight": 0.94,
-            "url": "https://www.marmiton.org/recettes/recette_gateau-au-chocolat-fondant-rapide_166352.aspx",
-            "ingredients": {
-                "chocolat pâtissier": 33.5,
-                "farine": 8.5,
-                "sucre en poudre": 16.5,
-                "beurre + une noix pour le moule": 16.5,
-                "oeuf": 0.5,
-            },
+    "Gâteau au chocolat fondant rapide": {
+        "ingredients": {
+            "chocolat pâtissier": 200.0,
+            "farine": 50.0,
+            "sucre en poudre": 100.0,
+            "beurre ": 100.0,
+            "oeufs": 210.0,
         },
-        {
-            "weight": 0.94,
-            "url": "https://www.marmiton.org/recettes/recette_gateau-au-chocolat-des-ecoliers_20654.aspx",
-            "ingredients": {
-                "chocolat noir": 25,
-                "sucre en poudre": 25,
-                "farine": 12.5,
-                "levure chimique": 2.75,
-                "oeuf": 0.5,
-                "beurre doux": 15.5,
-            },
+        "nb_people": 6,
+        "score": 0.976,
+        "url": "https://www.marmiton.org/recettes/recette_gateau-au-chocolat-fondant-rapide_166352.aspx",
+    },
+    "Gâteau au chocolat des écoliers": {
+        "ingredients": {
+            "chocolat noir": 200.0,
+            "sucre en poudre": 200.0,
+            "farine": 100.0,
+            "levure chimique": 11.0,
+            "oeufs": 280.0,
+            "beurre doux": 125.0,
         },
-        {
-            "weight": 0.96,
-            "url": "https://www.marmiton.org/recettes/recette_petits-gateaux-tout-chocolat_36095.aspx",
-            "ingredients": {
-                "farine": 11,
-                "sucre": 11,
-                "oeuf": 0.33,
-                "crème dessert au chocolat": 22.5,
-                "beurre": 11,
-            },
+        "nb_people": 8,
+        "score": 0.976,
+        "url": "https://www.marmiton.org/recettes/recette_gateau-au-chocolat-des-ecoliers_20654.aspx",
+    },
+    "Petits gâteaux tout chocolat": {
+        "ingredients": {
+            "farine": 100.0,
+            "sucre": 100.0,
+            "oeufs": 210.0,
+            "crème dessert au chocolat": 200.0,
+            "beurre": 100.0,
+            "moules à muffin ou des caissettes en papier": 250.0,
         },
-    ],
-    "cuisineaz": [
-        {
-            "weight": 1.0,
-            "url": "https://www.cuisineaz.com/recettes/gateau-au-chocolat-simplissime-47467.aspx",
-            "ingredients": {
-                "farine": 13.33,
-                "beurre": 20.83,
-                "oeufs": 0.5,
-                "sel": 1,
-                "chocolat pâtissier": 20.83,
-                "sucre": 20.83,
-                "levure chimique": 2,
-            },
+        "nb_people": 9,
+        "score": 0.5862,
+        "url": "https://www.marmiton.org/recettes/recette_petits-gateaux-tout-chocolat_36095.aspx",
+    },
+    "Gâteau au chocolat (seulement pour les enfants!)": {
+        "ingredients": {
+            "sucre": 250.0,
+            "farine": 250.0,
+            "levure": 5.5,
+            "sucre vanillé": 11.0,
+            "oeufs": 350.0,
+            "crème dessert au chocolat": 180.0,
+            "beurre": 50.0,
         },
-        {
-            "weight": 0.9,
-            "url": "https://www.cuisineaz.com/recettes/gateau-fondant-au-chocolat-11395.aspx",
-            "ingredients": {
-                "chocolat pâtissier": 16.67,
-                "oeufs entiers": 0.5,
-                "farine de blé (+10 pour le moule)": 6.67,
-                "beurre doux (+10 g pour le moule)": 12.5,
-                "sucre en poudre": 16.67,
-                "levure chimique": 2,
-            },
-        },
-    ],
+        "nb_people": 8,
+        "score": 0.5794,
+        "url": "https://www.marmiton.org/recettes/recette_gateau-au-chocolat-seulement-pour-les-enfants_41649.aspx",
+    },
 }
